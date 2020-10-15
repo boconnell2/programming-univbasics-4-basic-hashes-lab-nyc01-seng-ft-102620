9d5b7d0c048ad393d3b84ba002516bfbe13fdfbf
Brendan O'Connell <bmoconnell15@gmail.com>
2020-10-15 02:40:10 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -7,10 +7,10 @@ def my_hash
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  new_hash = {:key ==> "Grace Hopper"}
 end
 
 
 def id_hash_generator(number)
-  # return a hash with a key :id assigned to the provided number
+  hash = { :id ==> number }
 end
